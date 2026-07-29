c13ecd7391a86bc84eaf6c9d4a664d008b1f9ef7
colesmith001 <cole.smith@sdgku.edu>
2026-07-28 17:19:42 -0700

    last commit

--- my-react-store/src/pages/Home.jsx
+++ my-react-store/src/pages/Home.jsx
@@ -10,7 +10,7 @@
         <h2>Crafted with Passion. Grown for Community.</h2>
         <p>
             <b>
-            Welcome to Farmer’s Harvest, where quality meets craftsmanship right at the heart of the market. What began as a simple stand offering fresh, organic oranges has grown into a vibrant destination for hand-curated goods.
+            Welcome to Cole's super farm, where quality meets craftsmanship right at the heart of the market. What began as a simple stand offering fresh, organic oranges has grown into a vibrant destination for hand-curated goods.
             <br />
             <br />
             Whether you’re stopping by for our rich, artisanal dark chocolates, picking up fresh local produce, or cozying up in our signature custom apparel, everything we offer is thoughtfully sourced and made with care.
